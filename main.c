fa574cdeb75c18d765bbc125a81188abb39035e4
cccmatlock <67501974+cccmatlock@users.noreply.github.com>
2020-11-17 01:51:09 -0600

    Update main.c

--- main.c
+++ main.c
@@ -7,47 +7,98 @@ CS 4273-5273
 Scientic Calculator Project
 */
 
-typedef struct PeriodicElement_struct
-{
-	int atomicNumber;
-	char elementName[30];
-	char chemicalSymbol[30];
-	char elementClass[30];
-	double atomicWeight;
-	int electronsPerShell[7];
-} PeriodicElement;
+int BasicAddition(int num1, int num2){
 
- 
+	int x = num1;
+	int y = num2;
+	int answer = 0;
+
+	answer = x + y;
+
+	return answer;
+}
 
 int main(void)
 {
 	
-	char userKey;
-
-	userKey = '-';
-
-
-
+	char userKey = '-';
+	char a = '-';
 
 	while (userKey != 'q')
 	{
-		printf("\n\n\t\tWelcome to Scientific Calculator Plus...please enter an integer....");
-		printf("\n\n\t\tPlease enter y to enter an integer or q to quit.\n");
-		scanf(" %c", &userKey);
-
+		printf("\n\nWelcome to Scientific Calculator Plus...please enter an integer....");
+		printf("\n\nPlease enter y to perform calculations or q to quit.\n");
+		scanf("%c", &userKey);
+		a = getchar();
 
 
 		if (userKey == 'y')
 		{
+			char userAction = '-';
+
+			while (userAction != 'c') {
+
+				int userInt1 = 0;
+				int userInt2 = 0;
+				int result = 0;
+
+				printf("\n\nEnter the first integer\n");
+				scanf(" %d", &userInt1);
+				a = getchar();
+
+				printf("\n\nFor addition enter +, to clear enter c\n");
+				scanf(" %c", &userAction);
+				a = getchar();
+
+				if (userAction == '+') {
+					
+					while (userAction != 'c'){
+					
+						printf("\n\nEnter the second integer\n");
+						scanf(" %d", &userInt2);
+						a = getchar();
+
+						result = BasicAddition(userInt1, userInt2);
+						printf("\n\n%d", result);						
+
+						while (userAction == '+') {
+							printf("\n\nFor addition enter +, to clear enter c\n");
+							scanf(" %c", &userAction);
+							a = getchar();
+
+							if (userAction == '+') {
+
+								printf("\n\nEnter the second integer\n");
+								scanf(" %d", &userInt2);
+								a = getchar();
+
+								result = BasicAddition(result, userInt2);
+								printf("\n\n%d", result);
+
+							}
+
+							else if (userAction == 'c') {
+								break;
+							}
+							
+						}
+					}
+					
+				}
+
+
+			}
+		
+		
 		}
 
 		else if (userKey == 'q')
 		{
-			printf("Quitting...");
+			printf("\n\nQuitting...");
 		}
 		else
 		{
-			printf("invalid command...");
+			printf("\n\ninvalid command...");
 		}
 
 
